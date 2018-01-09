7f08309b7be217e83f7cf24948786f631baa2aa1
zchs <mlzchs@163.com>
2018-01-09 21:30:09 +0800

    初次

---
+++ NC65COMMON/webinter/src/public/nc/pub/xu/servlet/ServletParam.java
@@ -0,0 +1,11 @@
+package nc.pub.xu.servlet;
+/**
+ * 维护Servlet需要的相关参数
+ * NC定制的servlet（IHttpServletAdaptor）如果要进行业务处理，必须提供相关的参数
+ * 例如用户名，密码，数据源名称等
+ * @author Administrator
+ *
+ */
+public interface ServletParam {
+
+}
